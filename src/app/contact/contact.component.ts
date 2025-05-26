7b16a862a8a31b3d921f8e55fe212c3840397899
SwatiTrivedi767 <trivediswati443@gmail.com>
2025-05-26 21:48:35 +0530

    #Changes

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,18 +1,27 @@
 import { Component } from '@angular/core';
+import emailjs from 'emailjs-com';
+import { NgForm } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  styleUrl: './contact.component.css',
+   imports: [FormsModule] 
 })
 export class ContactComponent {
   
-  onSubmit(form: any) {
-    if (form.valid) {
-      // Handle form submission, e.g., send data to server
-      console.log('Form Submitted!', form.value);
-    }
+  onSubmit(form: NgForm) {
+    if (form.invalid) return;
+
+    emailjs.send('service_l752g96', 'template_09oxlsp', form.value, 'Re2-coVdA1vvv09gC')
+      .then(() => {
+        alert('Message sent successfully!');
+        form.reset();
+      })
+      .catch((error) => {
+        alert('Failed to send message. Error: ' + JSON.stringify(error));
+      });
   }
 }
